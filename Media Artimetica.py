51b1e496fdc5536815fbafd494a02356ac67068c
Arthur Barroso Neiva <arthur.b.neiva@gmail.com>
2025-04-29 21:23:37 -0300

    Add files via upload

---
+++ Media Artimetica.py
@@ -0,0 +1,8 @@
+Valor1 = int(input('Digite o primeiro número: '))
+Valor2 = int(input('Digite o segundo número: '))
+Valor3 = int(input('Digite o terceiro número: '))
+Valor4 = int(input('Digite o quarto número: '))
+
+Ma = (Valor1 + Valor2 + Valor3 + Valor4) / 4
+
+print(f'A média aritimetica é {Ma}')
